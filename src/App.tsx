279a87b733fc36c278d0ad340b06cc327e6785c5
TatianeMarinho <devtatianemarinho@gmail.com>
2023-08-21 15:53:23 -0400

    inputs e butao da pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Recipes from './pages/recipes/Recipes';
 import RecipesDetails from './pages/recipeDetails/RecipeDetails';
 import RecipeInProgress from './pages/recipeInProgress/RecipeInProgress';
 import DoneRecipes from './pages/doneRecipes/DoneRecipes';
 import FavoriteRecipes from './pages/favoriteRecipes/FavoriteRecipes';
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Login /> } />
-      <Route path="/meals" element={ <Recipes /> } />
-      <Route path="/drinks" element={ <Recipes /> } />
-      <Route path="/meals/:id-da-receita" element={ <RecipesDetails /> } />
-      <Route path="/drinks/:id-da-receita" element={ <RecipesDetails /> } />
-      <Route path="/meals/:id-da-receita/in-progress" element={ <RecipeInProgress /> } />
-      <Route path="/drinks/:id-da-receita/in-progress" element={ <RecipeInProgress /> } />
-      <Route path="/done-recipes" element={ <DoneRecipes /> } />
-      <Route path="/drinks/favorite-recipes" element={ <FavoriteRecipes /> } />
-      <Route path="/drinks/profile" element={ <Profile /> } />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/meals" element={ <Recipes /> } />
+        <Route path="/drinks" element={ <Recipes /> } />
+        <Route path="/meals/:id-da-receita" element={ <RecipesDetails /> } />
+        <Route path="/drinks/:id-da-receita" element={ <RecipesDetails /> } />
+        <Route
+          path="/meals/:id-da-receita/in-progress"
+          element={ <RecipeInProgress /> }
+        />
+        <Route
+          path="/drinks/:id-da-receita/in-progress"
+          element={ <RecipeInProgress /> }
+        />
+        <Route path="/done-recipes" element={ <DoneRecipes /> } />
+        <Route path="/drinks/favorite-recipes" element={ <FavoriteRecipes /> } />
+        <Route path="/drinks/profile" element={ <Profile /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,29 @@
 function Login() {
   return (
-    <h1>login</h1>
+    <div>
+      <h2>LOGIN</h2>
+      <form action="submit">
+        <input
+          type="text"
+          id="email-input"
+          placeholder="Email"
+          data-testid="email-input"
+        />
+        <input
+          type="text"
+          id="password-input"
+          placeholder="Password"
+          data-testid="password-input"
+        />
+        <button
+          id="login-submit-btn"
+          data-testid="login-submit-btn"
+        >
+          Enter
+        </button>
+      </form>
+
+    </div>
   );
 }
 export default Login;
