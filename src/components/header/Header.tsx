e2f635366c7519f20e851613c4111c00e0903743
joaoluizc <joaocoelho.tdm@gmail.com>
2023-08-22 16:45:30 -0300

    implementando barra de busca no header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import searchIcon from '../../images/searchIcon.svg';
+import profileIcon from '../../images/profileIcon.svg';
+import SearchBar from '../searchBar/SearchBar';
+
 function Header() {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
   return (
     <header>
-      <img src="/src/images/profileIcon.svg" alt="icone de perfil" />
+      <img src={ profileIcon } alt="icone de perfil" />
+      <button
+        onClick={ () => setShowSearchBar(!showSearchBar) }
+        data-testid="search-top-btn"
+      >
+        <img src={ searchIcon } alt="icone de pesquisa" />
+      </button>
+      { showSearchBar && <SearchBar />}
     </header>
   );
 }
--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,6 +1,13 @@
 function SearchBar() {
   return (
-    <h1>Search Bar</h1>
+    <>
+      <h1>Search Bar</h1>
+      <input
+        type="text"
+        name="searchBar"
+        data-testid="search-input"
+      />
+    </>
   );
 }
 export default SearchBar;
