11a5a20cb1e33bce8b3f8e1a8627fc7d4fe3a99d
TatianeMarinho <devtatianemarinho@gmail.com>
2023-08-23 18:55:22 -0400

    butões do footer implementados

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,25 @@
+import drinkIcon from '../../images/drinkIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
+import './footer.css';
+
 function Footer() {
   return (
-    <h1>iniciando o footer</h1>
+    <footer id="footer" data-testid="footer">
+      <button>
+        <img
+          src={ drinkIcon }
+          alt="butao de drink"
+          data-testid="drinks-bottom-btn"
+        />
+      </button>
+      <button>
+        <img
+          src={ mealIcon }
+          alt="icone de drink"
+          data-testid="meals-bottom-btn"
+        />
+      </button>
+    </footer>
   );
 }
 export default Footer;
