d9c2fb8ddf8a797bc8ac4fe2f8cddfaed930d8c8
TatianeMarinho <devtatianemarinho@gmail.com>
2023-08-24 13:10:05 -0400

    funcionalidade dos botões

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import drinkIcon from '../../images/drinkIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 import './footer.css';
 
 function Footer() {
+  const navigate = useNavigate();
+
   return (
     <footer
       className="footer"
       data-testid="footer"
     >
-      <button>
+      <button onClick={ () => navigate('/drinks') }>
         <img
           src={ drinkIcon }
           alt="drink icon"
           data-testid="drinks-bottom-btn"
         />
       </button>
-      <button>
+      <button onClick={ () => navigate('/meals') }>
         <img
           src={ mealIcon }
           alt="meal icon"
