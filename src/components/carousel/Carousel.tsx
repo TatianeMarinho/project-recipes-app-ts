75c7e33bf134da32bb55bbab55738fb112a8b522
Rodrigo Silva <rodrigosasilva99@gmail.com>
2023-08-30 16:57:09 -0300

    carousel finalizado

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -10,45 +10,52 @@ type CarouselType = {
 
 export default function CarouselCard(props: CarouselType) {
   const { drinksRecomendaded, mealsRecomendaded } = props;
+  console.log(drinksRecomendaded);
+  console.log(mealsRecomendaded);
 
-  if (drinksRecomendaded) {
-    return (
-      <div>
-        <h4>React-Bootstrap Carousel Component</h4>
-        <Carousel>
-          {drinksRecomendaded.map((drink, index) => (
-            <Carousel.Item interval={ 1500 } key={ drink.idDrink }>
-              <div data-testid={ `${index}-recommendation-card` }>
-                <img
-                  src={ drink.strDrinkThumb }
-                  alt={ drink.strDrink }
-                  data-testid={ `${index}-recommendation-title` }
-                />
-              </div>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      </div>
-    );
-  }
-  if (mealsRecomendaded) {
-    return (
-      <div>
-        <h4>React-Bootstrap Carousel Component</h4>
-        <Carousel>
+  return (
+    <div>
+      <h4>Recommended recipes</h4>
+      {(mealsRecomendaded) && (
+        <Carousel data-bs-theme="dark">
           {mealsRecomendaded.map((meal, index) => (
-            <Carousel.Item interval={ 1500 } key={ meal.idMeal }>
-              <div data-testid={ `${index}-recommendation-card` }>
-                <img
-                  src={ meal.strMealThumb }
-                  alt={ meal.strMeal }
-                  data-testid={ `${index}-recommendation-title` }
-                />
-              </div>
+            <Carousel.Item
+              interval={ 1500 }
+              key={ meal.idMeal }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <img
+                src={ meal.strMealThumb }
+                alt={ meal.strMeal }
+              />
+              <Carousel.Caption>
+                <h3 data-testid={ `${index}-recommendation-title` }>{meal.strMeal}</h3>
+                <p>{meal.strCategory}</p>
+              </Carousel.Caption>
             </Carousel.Item>
           ))}
         </Carousel>
-      </div>
-    );
-  }
+      )}
+      {(drinksRecomendaded) && (
+        <Carousel data-bs-theme="dark">
+          {drinksRecomendaded.map((drink, index) => (
+            <Carousel.Item
+              interval={ 1500 }
+              key={ drink.idDrink }
+              data-testid={ `${index}-recommendation-card` }
+            >
+              <img
+                src={ drink.strDrinkThumb }
+                alt={ drink.strDrink }
+              />
+              <Carousel.Caption>
+                <h3 data-testid={ `${index}-recommendation-title` }>{drink.strDrink}</h3>
+                <p>{drink.strAlcoholic}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )}
+    </div>
+  );
 }
