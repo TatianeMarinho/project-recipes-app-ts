6eca0f44582d96b2ef10c27d8e5d4e8e3fb77921
joaoluizc <joaocoelho.tdm@gmail.com>
2023-09-02 23:35:18 -0300

    comecando testes de Recipes

--- src/tests/Recipes.test.tsx
+++ src/tests/Recipes.test.tsx
@@ -1,59 +1,23 @@
-// import App from "../App";
-// import { renderWithRouter } from "./helpers/renderWith";
-// import userEvent from '@testing-library/user-event';
-// import { fireEvent, render, screen } from '@testing-library/react';
-// import { vi } from "vitest";
-// import foodCategoriesMock from "./helpers/foodCategoriesMock";
-// import { fireEvent, render } from "@testing-library/react";
-// import Recipes from "../pages/recipes/Recipes";
-
-// describe('testando componente recipes', () => {
-//   test('testando a funçao handleClickAll', async () => {
-//     renderWithRouter(<App />, { initialEntries: ['/drinks'] });
-
-//     const btnAll = screen.getByTestId('All-category-filter');
-//     expect(btnAll).toBeInTheDocument()
-//     await userEvent.click(btnAll);
-//   })
-// })
-
-
+import userEvent from '@testing-library/user-event';
+import { screen, waitFor } from '@testing-library/react';
 import { vi } from 'vitest';
-import initialFoodMock from './helpers/initialFoodMock';
-import initialDrinkMock from './helpers/initialDrinkMock';
-import foodCategoriesMock from './helpers/foodCategoriesMock';
-import drinkCategoriesMock from './helpers/drinkCategoriesMock';
 import { renderWithRouter } from './helpers/renderWith';
-import { screen } from '@testing-library/dom';
 import App from '../App';
 
+describe('Testa a página Recipes', () => {
+  beforeEach(() => {
+    global.fetch = vi.fn().mockImplementation(fetchMock);
+    window.alert = vi.fn(() => {});
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  test('Testa se todos os botões de filtro aparecem', async () => {
+    renderWithRouter(<App />, { initialEntries: ['/meals'] });
 
-describe('Recipes component behaves as expected', async () => {
-  test('test', () => {
-    const fetch = (url: string) => Promise.resolve({
-      status: 200,
-      ok: true,
-      json: () => {
-        if (url === 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'){
-          return Promise.resolve(foodCategoriesMock);
-        }
-        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'){
-          return Promise.resolve(drinkCategoriesMock);
-        }
-        if (url === 'https://www.themealdb.com/api/json/v1/1/search.php?s='){
-          return Promise.resolve(initialFoodMock);
-        }
-        if (url === 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='){
-          return Promise.resolve(initialDrinkMock);
-        }
-      },
-    });
-        
-    vi.spyOn(global, 'fetch').mockImplementation(fetch);
-  
-  
-    renderWithRouter(<App />, { initialEntries: ['/drinks'] });
-    screen.debug();
-  })
-
+    waitFor(() => screen.findByText('Corba'));
+    const btnAll = screen.getByTestId('All-category-filter');
+    expect(btnAll).toBeInTheDocument();
+    await userEvent.click(btnAll);
+  });
 });
